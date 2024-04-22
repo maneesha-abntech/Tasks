c0fe3ecd15e11ff60f4dbe97b0c60a04ccc7eeeb
maneesha <maneesha@abntechcorp.com>
2024-04-22 21:34:59 +0530

    added main.py file

--- get.py
+++ get.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+"""from fastapi import FastAPI
 
 app = FastAPI()
 
@@ -6,3 +6,23 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+"""
+#post method
+from fastapi import FastAPI
+from pydantic import BaseModel
+app=FastAPI()
+
+class itemA(BaseModel):
+    name:str
+    description:str
+@app.post("/items")
+def create_items(item_a:itemA):
+    print(item_a)
+    return{"item_a":item_a}
+
+
+
+
+
+
+
